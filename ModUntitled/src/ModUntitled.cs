24c7e4c663c11d5309a35457665bf475d8553d3d
Zatherz <zatherz@linux.pl>
2018-12-21 19:41:07 +0100

    Working initialization

---
+++ ModUntitled/src/ModUntitled.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using ModTheGungeon;
+using UnityEngine;
+using Logger = ModTheGungeon.Logger;
+
+namespace ModUntitled {
+    public class ModUntitled : MonoBehaviour {
+        public static Logger Logger = new Logger("ModUntitled");
+        public static GameObject GameObject;
+        public static ModUntitled Instance;
+
+        public void GamePreload() {
+            Logger.Info("Game Preload");
+        }
+
+        public void GameLoad() {
+            Logger.Info("Game Load");
+        }
+    }
+}
---
+++ ModUntitled/src/Patches/GameManager.cs
@@ -0,0 +1,21 @@
+﻿#pragma warning disable 0626
+#pragma warning disable 0649
+
+using System;
+using ModTheGungeon;
+using UnityEngine;
+using System.Reflection;
+using MonoMod;
+
+namespace ModUntitled.Patches {
+    [MonoModPatch("global::GameManager")]
+    internal class GameManager : global::GameManager {
+        protected extern void orig_Awake();
+        private new void Awake() {
+            ModUntitled.Logger.Info("GameManager is alive");
+            ModUntitled.Instance.GameLoad();
+
+            orig_Awake();
+        }
+    }
+}
